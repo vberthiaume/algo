b70779c1edf58a58a80be4462a9c6edaa7507c64
vberthiaume <vberthiaume@gmail.com>
2015-10-09 15:59:37 -0400

    all code with values

--- week1/mergeSort.cpp
+++ week1/mergeSort.cpp
@@ -3,28 +3,31 @@
 
 using namespace std;
 
-void mergeSort(vector<int> &v){
+vector<int> mergeSort(vector<int> &v){
     int iCurSize = v.size();
     if (iCurSize > 2){
         vector<int> v1 (&v[0], &v[iCurSize/2]);
         vector<int> v2 (&v[iCurSize/2 +1], &v[iCurSize]);
         mergeSort(v1); 
         mergeSort(v2); 
+        v1.insert(v1.end(), v2.begin(), v2.end());
+        return v1;
     } else {
         if (v[0] > v[1]){
             int iTemp = v[1];
             v[1] = v[0];
             v[0] = iTemp;
         }
+        return v;
     }
 }
 
 int main(){
-    vector<int> v{4,5,7,3,2,8,7,1};
-    mergeSort(v);
+    vector<int> v_in{4,5,7,3,2,8,7,1};
+    auto v_out = (v_in);
     
     cout << "("; 
-    for (auto i : v){
+    for (auto i : v_out){
         cout << i << ",";
     }
     cout << ")\n";
