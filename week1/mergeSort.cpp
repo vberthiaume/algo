9b76968938067acff61d3f1216e2f5f34e347c56
vberthiaume <vberthiaume@gmail.com>
2015-10-09 16:22:07 -0400

    better

--- week1/mergeSort.cpp
+++ week1/mergeSort.cpp
@@ -6,17 +6,28 @@ using namespace std;
 vector<int> mergeSort(vector<int> &v){
     int iCurSize = v.size();
     if (iCurSize > 2){
-        vector<int> v1 (&v[0], &v[iCurSize/2]);
-        vector<int> v2 (&v[iCurSize/2 +1], &v[iCurSize]);
+        //vector<int> v1 (&v[0], &v[iCurSize/2-1]);
+        //vector<int> v2 (&v[iCurSize/2], &v[iCurSize-1]);
+
+        vector<int> v1(v.begin(), v.begin() + iCurSize/2);
+        vector<int> v2(v.begin()+iCurSize/2, v.end());
+
+
         mergeSort(v1); 
         mergeSort(v2); 
         v1.insert(v1.end(), v2.begin(), v2.end());
+        for (auto i : v1){
+            cout << i;
+        }
+        cout << endl;
         return v1;
     } else {
         if (v[0] > v[1]){
+            cout << "b: " << v[0] << v[1] << "\n";
             int iTemp = v[1];
             v[1] = v[0];
             v[0] = iTemp;
+            cout << "a: " << v[0] << v[1] << "\n";
         }
         return v;
     }
@@ -24,7 +35,8 @@ vector<int> mergeSort(vector<int> &v){
 
 int main(){
     vector<int> v_in{4,5,7,3,2,8,7,1};
-    auto v_out = (v_in);
+    auto v_out = mergeSort(v_in);
+     
     
     cout << "("; 
     for (auto i : v_out){
