d363ecae132ec360ac5424060c7d496911bd9cb9
vberthiaume <vberthiaume@gmail.com>
2015-10-09 15:01:36 -0400

    mergeSort runs

--- week1/mergeSort.cpp
+++ week1/mergeSort.cpp
@@ -1,8 +1,20 @@
+#include <iostream>
 #include <vector>
 
 using namespace std;
+
+vector<int> mergeSort(vector<int> v){
+    return v;
+}
+
 int main(){
-    vector<int> v;
-
-
+    vector<int> v_in{4,5,7,3,2,8,7,1};
+    vector<int> v_out = mergeSort(v_in);
+    
+    cout << "("; 
+    for (auto i : v_out){
+        cout << i << ",";
+    }
+    cout << ")\n";
+    return 0;
 }
